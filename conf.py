78c6a51fec7c9f33d29732caac2b33aba8ac39e1
ayagawaseirin <461514286@qq.com>
2020-03-10 10:17:24 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,19 +14,19 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "AyagawaSeirin/diary@gh-pages"
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Seirin's Diary"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "AyagawaSeirin"
+email = "AyagawaSeirin@outlook.com"
+author_homepage = "https://moegirl.life"
+description = "皮皮凛的个人日记站点"
+key_words = ['皮皮凛', '个人日记', 'AyagawaSeirin', '绫川星凛']
 language = 'zh-CN'
 external_links = [
     {
